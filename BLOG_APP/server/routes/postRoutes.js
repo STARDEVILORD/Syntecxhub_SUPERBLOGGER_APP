c402d1affaa0a77ef0d5ce8e23cef3e92c332ac2
SHRIRAM <shriramuday12345610@gmail.com>
2026-07-03 17:44:34 +0530

    Update postRoutes.js

--- BLOG_APP/server/routes/postRoutes.js
+++ BLOG_APP/server/routes/postRoutes.js
@@ -9,14 +9,10 @@ const {
   toggleLike,
   getUserPosts,
 } = require("../controllers/postController");
-
-// ⚠️ IMPORTANT: User posts route BEFORE /:id
 router.get("/user/:userId", auth, getUserPosts);
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-
-// ⚠️ Make sure createPost has auth middleware
 router.post("/", auth, createPost);
 router.put("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
