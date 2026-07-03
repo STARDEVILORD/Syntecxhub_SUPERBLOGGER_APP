c21b3faaf27b4122014e9bd95feda665c9ef61a6
SHRIRAM <shriramuday12345610@gmail.com>
2026-07-03 17:25:08 +0530

    Update postRoutes.js

--- BLOG_APP/server/routes/postRoutes.js
+++ BLOG_APP/server/routes/postRoutes.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
 const {
+  createPost,
   getPosts,
   getPost,
-  createPost,
   updatePost,
   deletePost,
   toggleLike,
   getUserPosts,
 } = require("../controllers/postController");
 
+// ⚠️ IMPORTANT: User posts route BEFORE /:id
 router.get("/user/:userId", auth, getUserPosts);
+
 router.get("/", getPosts);
 router.get("/:id", getPost);
+
+// ⚠️ Make sure createPost has auth middleware
 router.post("/", auth, createPost);
 router.put("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
